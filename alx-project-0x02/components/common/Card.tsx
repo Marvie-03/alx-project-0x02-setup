ae98b5ba92bc3439d372caf8bc95feda11b2dec1
Modupe Marvellous <101948687+Marvie-03@users.noreply.github.com>
2025-09-26 23:07:57 +0000

    Task 2&3

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,20 @@
+import { CardProps } from "@/interfaces";
+
+const Card: React.FC<CardProps> = ({ id, title, content }) => {
+    return (
+        <div className="flex flex-col w-1/3" key={id}>
+            <main className="flex-grow flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
+                <div className="text-center">
+                    <h1 className="text-3xl font-bold text-white">
+                        {title}
+                    </h1>
+                    <p className="mt-4 text-xl text-white">
+                        {content}
+                    </p>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default Card;
